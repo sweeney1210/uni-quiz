ce1e67b154ac8485e44766bd377076b9e89964f6
sweeney1210 <64714227+sweeney1210@users.noreply.github.com>
2022-04-27 22:14:52 +0100

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,128 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the capital of Albania?",
+    answer: "Tirana",
+    options: [
+      "Pristina",
+      "Tirana",
+      "Sofia",
+      "Tripoli"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the capital of Equitorial Guinea?",
+    answer: "Malabo",
+    options: [
+      "Malabo",
+      "Libreville",
+      "Gaborone",
+      "Maseru"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the capital of Belize?",
+    answer: "Belmopan",
+    options: [
+      "Belize City",
+      "Belmopan",
+      "Orange Walk",
+      "Merida"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the capital of Kyrgyzstan?",
+    answer: "Bishkek",
+    options: [
+      "Almaty",
+      "Ashgabat",
+      "Tashkent",
+      "Bishkek"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the capital of Sierra Leone?",
+    answer: "Freetown",
+    options: [
+      "Conakry",
+      "Antananarivo",
+      "Freetown",
+      "Lilongwe"
+    ]
+  },
+    {
+    numb: 6,
+    question: "How many capital cities does South Africa have?",
+    answer: "3",
+    options: [
+      "1",
+      "3",
+      "2",
+      "4"
+    ]
+  },
+    {
+    numb: 7,
+    question: "What is the capital city of Jordan?",
+    answer: "Amman",
+    options: [
+      "Oman",
+      "Amman",
+      "Erbil",
+      "Medina"
+    ]
+  },
+    {
+    numb: 8,
+    question: "What nationality are the people of Greenland?",
+    answer: "Danish",
+    options: [
+      "Danish",
+      "Norweigan",
+      "Greenlanders",
+      "Icelandic"
+    ]
+    },
+    {
+    numb: 9,
+    question: "What state in the USA is furtherst west?",
+    answer: "Alaska",
+    options: [
+      "Hawaii",
+      "California",
+      "Alaska",
+      "Maine"
+    ]
+    },
+    {
+    numb: 10,
+    question: "You are unable to drive between North and South America because of the Darien Gap, but what two countries is the Darien Gap located in?",
+    answer: "Panama & Colombia",
+    options: [
+      "Panama & Ecuador",
+      "Costa Rica & Venezuela",
+      "Panama & Colombia",
+      "Honduras & Colombia"
+    ]
+    },
+
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
